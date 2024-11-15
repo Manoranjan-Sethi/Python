2ecb15ac8beec2d4076cf1944571adeb1cffd772
Manoranjan Sethi <manoranjansethi97@gmail.com>
2024-11-16 00:45:10 +0530

    np list

---
+++ Day-2/Non-Primitive.py
@@ -0,0 +1,13 @@
+"""
+Lists - Combination of data types
+      - Should have a square bracket
+"""
+
+list_1 = ["Mano", 23, True, 78.09, None, False]
+print(list_1)
+print(type(list_1))
+
+fruit_list = ["apple", "orange", "Banana"]
+num_list = [2, 3, 4, 5, 6]
+
+
