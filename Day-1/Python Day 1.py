64bbd6f56b9b130e0490c64e730af6de508c770d
Manoranjan Sethi <manoranjansethi97@gmail.com>
2024-11-15 19:42:20 +0530

    D1 basics

---
+++ Day-1/Python Day 1.py
@@ -0,0 +1,41 @@
+# Python
+
+"""
+High-level language(HLL)
+Interpreted language (HLL converted into LLL)
+Code executed is line by line for which debugging and finding errors is easy
+"""
+
+# Single Line Comment
+
+"""Multi
+Line Comment"""
+
+"""
+Extension of Python
+The extension of Python is .py but in jupyter file format is .ipynb (Interactive Python Notebook)
+"""
+
+"""
+Python Keywords and Identifiers
+
+Keywords - Reserved words (can't be used as identifiers)
+Identifiers - Assigning own variables
+"""
+
+""" Data Types
+
+Primitive Types (Homogenous) - identifier has one value -ex- integer, float, string, boolean, complex, None
+Non-primitive types (Hetrogeneous) - identifier has multiple values/objects -ex- list, tuple, dictionary, set
+"""
+
+print("Hello World")
+
+# Integer
+
+a = 5
+print(a)
+
+b = a-10
+print(b)
+print(type(b))
