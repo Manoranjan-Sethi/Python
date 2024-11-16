3377fdc53118a519c113cfec25f3d187c6e5e144
Manoranjan Sethi <manoranjansethi97@gmail.com>
2024-11-17 00:50:17 +0530

    np list

--- Day-2/Non-Primitive.py
+++ Day-2/Non-Primitive.py
@@ -7,7 +7,35 @@ list_1 = ["Mano", 23, True, 78.09, None, False]
 print(list_1)
 print(type(list_1))
 
-fruit_list = ["apple", "orange", "Banana"]
 num_list = [2, 3, 4, 5, 6]
+print(num_list[2])
+
+fruit_list = ["apple", "orange", "Banana"]
+print((fruit_list)[-1])  #backward indexing 
 
 
+# Nested lists
+
+nlist = [1,2,3,["a","b","c","d","e","f","g","h"],7,8,9]
+print(nlist[3][2])  # c
+
+nlist = [1,2,3,["a","b","c","d",["e","f"],"g","h"],7,8,9]
+print(nlist[3][4][1]) # f
+
+# Method will always have ".methodname(xyz)"; dedicated for a particular class/data type
+
+# Append Method - adds one element by the end of the list
+
+fruit_list = ["apple", "orange", "Banana"]
+fruit_list.append("ice apple")
+print(fruit_list)
+
+# Extend Method - adds multiple element by the end of the list
+
+fruit_list.extend(["watermelon","dates","dragon fruit"])
+print(fruit_list)
+
+# insert element in the middle of the list
+
+fruit_list.insert(3,"berry") # .insert(index,"name"/number)
+print(fruit_list)
