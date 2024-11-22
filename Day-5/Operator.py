93ba927a8e6c3ef569dce92a687304a4009a9b13
Manoranjan Sethi <manoranjansethi97@gmail.com>
2024-11-22 13:00:57 +0530

    ope

--- Day-5/Operator.py
+++ Day-5/Operator.py
@@ -1,4 +1,20 @@
 '''
 Operators -> Arithmetic Operator - +, -, *, /, //, **, %
- 
+          -> Assignment operator - =, +=, -=, *=, %=, //=, /=
+          -> Conditional operator - ==, !=, >, < <, >=, <=
 '''
+
+x=11
+y=30
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(y/x)
+print("floor",y//x)
+print("mod",y%x)
+print(y**x)
+
+x+=4
+print(x)
